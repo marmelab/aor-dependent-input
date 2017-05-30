4e266c60297908fb5a89516a0f7c06c6a8825919
gildas <gildas.garcia@gmail.com>
2017-05-30 10:54:47 +0200

    added tests

--- src/getValue.js
+++ src/getValue.js
@@ -1,10 +1,6 @@
 import get from 'lodash.get';
 
 export default (value, path) => {
-    if (path.includes('.')) {
-        return get(value, path);
-    }
-
     if (typeof value === 'object') {
         return get(value, path);
     }
