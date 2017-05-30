b5993436033748f5716795d354a455338b5be1d5
gildas <gildas.garcia@gmail.com>
2017-05-30 09:45:22 +0200

    [RFR] DependentField

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![Build Status](https://travis-ci.org/marmelab/aor-dependent-input.svg?branch=master)](https://travis-ci.org/marmelab/aor-dependent-input)
 
-A component for displaying input depending on other inputs values in [Admin-on-rest](https://github.com/marmelab/admin-on-rest).
+A component for displaying inputs and fields depending on other inputs or fields values in [Admin-on-rest](https://github.com/marmelab/admin-on-rest).
 
 - [Installation](#installation)
 - [Usage](#installation)
@@ -27,7 +27,7 @@ yarn add aor-dependent-input
 Check that the field specified by `dependsOn` has a value (a truthy value):
 
 ```js
-import DependentInput from 'aor-dependent-input';
+import { DependentInput } from 'aor-dependent-input';
 
 export const UserCreate = (props) => (
     <Create {...props}>
@@ -46,7 +46,7 @@ export const UserCreate = (props) => (
 Check that the field specified by `dependsOn` has a specific value:
 
 ```js
-import DependentInput from 'aor-dependent-input';
+import { DependentInput } from 'aor-dependent-input';
 
 export const PostCreate = (props) => (
     <Create {...props}>
@@ -86,7 +86,7 @@ export const PostCreate = (props) => (
 Check that the field specified by `dependsOn` matches a custom constraint:
 
 ```js
-import DependentInput from 'aor-dependent-input';
+import { DependentInput } from 'aor-dependent-input';
 
 const checkCustomConstraint = (value) => value.startsWith('programming'));
 
@@ -117,7 +117,7 @@ export const PostCreate = (props) => (
 All powers! Check whether the current full record matches your constraints:
 
 ```js
-import DependentInput from 'aor-dependent-input';
+import { DependentInput } from 'aor-dependent-input';
 
 const checkRecord = (record) => record.firstName && record.lastName);
 
@@ -137,21 +137,21 @@ export const UserCreate = (props) => (
 
 ## API
 
-The `DependentInput` accepts the following props:
+The `DependentInput` and `DependentField` components accepts the following props:
 
 ### dependsOn
 
 Either a string indicating the name of the field to check (eg: `hasEmail`) or an array of fields to check (eg: `['firstName', 'lastName']`).
 
-`DependentInput` uses the [formValueSelector](http://redux-form.com/6.7.0/docs/api/FormValueSelector.md) from `redux-form` to retrieve the value(s). That means you can specify deep paths such as `author.firstName`.
+You can specify deep paths such as `author.firstName`.
 
 ### value
 
-If not specified, `DependentInput` will only check that the field(s) specified by `dependsOn` have a truthy value.
+If not specified, only check that the field(s) specified by `dependsOn` have a truthy value.
 
 You may specify a single value or an array of values. Deep paths will be correctly retrieved and compared to the specified values.
 
-If both `value` and `record` are specified, `value` will be ignored.
+If both `value` and `resolve` are specified, `value` will be ignored.
 
 ### resolve
 
@@ -159,15 +159,15 @@ The `resolve` prop accepts a function which must return either `true` to display
 
 If the `dependsOn` prop is specified, `resolve` will be called with either the value of the field specified by `dependsOn` (when a single field name was specified as `dependsOn`) or with an object matching the specified paths.
 
-**Note**: When specifying deep paths (eg: `author.firstName`), `redux-form` will return an object with a matching structure. For example, when passing `['author.firstName', 'author.lastName']` as `dependsOn`, the `value` function will be passed the following object:
+**Note**: When specifying deep paths (eg: `author.firstName`), `resolve` will be called with an object matching the specified structure. For example, when passing `['author.firstName', 'author.lastName']` as `dependsOn`, the `resolve` function will be passed the following object:
 
 ```js
 { author: { firstName: 'bValue', lastName: 'cValue' } }
 ```
 
-If `dependsOn` is not specified, `resolve` will be called with the current form values (full record).
+If `dependsOn` is not specified, `resolve` will be called with the current form values (`DependentInput`) or the full record (`DependentField`).
 
-If both `value` and `record` are specified, `value` will be ignored.
+If both `value` and `resolve` are specified, `value` will be ignored.
 
 ## Contributing
 
---
+++ src/DependentInput.spec.js
@@ -0,0 +1,390 @@
+import React from 'react';
+import expect, { createSpy } from 'expect';
+import { shallow } from 'enzyme';
+import { DependentInputComponent as DependentInput, mapStateToProps } from './DependentInput';
+
+describe('mapStateToProps', () => {
+    describe('with resolve function', () => {
+        it('returns { show: false } if the resolve returns false', () => {
+            const resolve = createSpy().andReturn(false);
+            expect(mapStateToProps({}, { resolve })).toEqual({ show: false });
+        });
+
+        it('returns { show: true } if the resolve returns true', () => {
+            const resolve = createSpy().andReturn(true);
+            expect(mapStateToProps({}, { resolve })).toEqual({ show: true });
+        });
+    });
+
+    describe('with only dependsOn specified as a string', () => {
+        it('returns { show: false } if the form does not have a truthy value for the field matching dependsOn', () => {
+            expect(
+                mapStateToProps(
+                    {
+                        form: {
+                            'record-form': {
+                                values: {
+                                    lastName: 'blublu',
+                                },
+                            },
+                        },
+                    },
+                    {
+                        dependsOn: 'firstName',
+                    },
+                ),
+            ).toEqual({ show: false });
+        });
+
+        it('returns { show: true } if the form has a truthy value for the field matching dependsOn', () => {
+            expect(
+                mapStateToProps(
+                    {
+                        form: {
+                            'record-form': {
+                                values: {
+                                    firstName: 'blublu',
+                                },
+                            },
+                        },
+                    },
+                    { dependsOn: 'firstName' },
+                ),
+            ).toEqual({ show: true });
+        });
+    });
+
+    describe('with only dependsOn specified as a deep path string', () => {
+        it('returns { show: false } if the form does not have a truthy value for the field matching dependsOn', () => {
+            expect(
+                mapStateToProps(
+                    {
+                        form: {
+                            'record-form': {
+                                values: {
+                                    author: {
+                                        lastName: 'blublu',
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    { dependsOn: 'author.firstName' },
+                ),
+            ).toEqual({ show: false });
+        });
+
+        it('returns { show: true } if the form has a truthy value for the field matching dependsOn', () => {
+            expect(
+                mapStateToProps(
+                    {
+                        form: {
+                            'record-form': {
+                                values: {
+                                    author: {
+                                        firstName: 'blublu',
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    { dependsOn: 'author.firstName' },
+                ),
+            ).toEqual({ show: true });
+        });
+    });
+
+    describe('with dependsOn specified as a string and a specific value', () => {
+        it('returns { show: false } if the form does not have the specific value for the field matching dependsOn', () => {
+            expect(
+                mapStateToProps(
+                    {
+                        form: {
+                            'record-form': {
+                                values: {
+                                    firstName: 'bar',
+                                },
+                            },
+                        },
+                    },
+                    { dependsOn: 'firstName', value: 'foo' },
+                ),
+            ).toEqual({ show: false });
+        });
+
+        it('returns { show: true } if the form have the specific value for the field matching dependsOn', () => {
+            expect(
+                mapStateToProps(
+                    {
+                        form: {
+                            'record-form': {
+                                values: {
+                                    firstName: 'foo',
+                                },
+                            },
+                        },
+                    },
+                    { dependsOn: 'firstName', value: 'foo' },
+                ),
+            ).toEqual({ show: true });
+        });
+    });
+
+    describe('with dependsOn specified as a string and resolve', () => {
+        it('returns { show: false } if the resolve function returns false', () => {
+            expect(
+                mapStateToProps(
+                    {
+                        form: {
+                            'record-form': {
+                                values: {
+                                    firstName: 'bar',
+                                },
+                            },
+                        },
+                    },
+                    { dependsOn: 'firstName', resolve: value => value === 'foo' },
+                ),
+            ).toEqual({ show: false });
+        });
+
+        it('returns { show: true } if the resolve function returns true', () => {
+            expect(
+                mapStateToProps(
+                    {
+                        form: {
+                            'record-form': {
+                                values: {
+                                    firstName: 'foo',
+                                },
+                            },
+                        },
+                    },
+                    { dependsOn: 'firstName', resolve: value => value === 'foo' },
+                ),
+            ).toEqual({ show: true });
+        });
+    });
+
+    describe('with only dependsOn specified as an array', () => {
+        it('returns { show: false } if the form does not have a truthy value for the fields matching dependsOn', () => {
+            expect(
+                mapStateToProps(
+                    {
+                        form: {
+                            'record-form': {
+                                values: {
+                                    lastName: 'blublu',
+                                },
+                            },
+                        },
+                    },
+                    { dependsOn: ['firstName', 'lastName'] },
+                ),
+            ).toEqual({ show: false });
+        });
+
+        it('returns { show: true } if the form has a truthy value for the fields matching dependsOn', () => {
+            expect(
+                mapStateToProps(
+                    {
+                        form: {
+                            'record-form': {
+                                values: {
+                                    firstName: 'blublu',
+                                    lastName: 'blublu',
+                                },
+                            },
+                        },
+                    },
+                    { dependsOn: ['firstName', 'lastName'] },
+                ),
+            ).toEqual({ show: true });
+        });
+    });
+
+    describe('with only dependsOn specified as an array with deep path strings', () => {
+        it('returns { show: false } if the form does not have a truthy value for the fields matching dependsOn', () => {
+            expect(
+                mapStateToProps(
+                    {
+                        form: {
+                            'record-form': {
+                                values: {
+                                    date: new Date().toDateString(),
+                                    author: {
+                                        lastName: 'blublu',
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    { dependsOn: ['author.firstName', 'date'] },
+                ),
+            ).toEqual({ show: false });
+        });
+
+        it('returns { show: true } if the form has a truthy value for the fields matching dependsOn', () => {
+            expect(
+                mapStateToProps(
+                    {
+                        form: {
+                            'record-form': {
+                                values: {
+                                    date: new Date().toDateString(),
+                                    author: {
+                                        firstName: 'blublu',
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    { dependsOn: ['author.firstName', 'date'] },
+                ),
+            ).toEqual({ show: true });
+        });
+    });
+
+    describe('with dependsOn specified as an array and specific values as an array', () => {
+        it('returns { show: false } if the form does not have the specific values for the fields matching dependsOn', () => {
+            expect(
+                mapStateToProps(
+                    {
+                        form: {
+                            'record-form': {
+                                values: {
+                                    category: 'bar',
+                                    author: {
+                                        firstName: 'bar',
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    {
+                        dependsOn: ['author.firstName', 'category'],
+                        value: ['foo', 'bar'],
+                    },
+                ),
+            ).toEqual({ show: false });
+        });
+
+        it('returns { show: true } if the form have the specific values for the fields matching dependsOn', () => {
+            expect(
+                mapStateToProps(
+                    {
+                        form: {
+                            'record-form': {
+                                values: {
+                                    category: 'bar',
+                                    author: {
+                                        firstName: 'foo',
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    {
+                        dependsOn: ['author.firstName', 'category'],
+                        value: ['foo', 'bar'],
+                    },
+                ),
+            ).toEqual({ show: true });
+        });
+    });
+
+    describe('with dependsOn specified as an array and resolve', () => {
+        it('returns { show: false } if the resolve function returns false', () => {
+            expect(
+                mapStateToProps(
+                    {
+                        form: {
+                            'record-form': {
+                                values: {
+                                    category: 'bar',
+                                    author: {
+                                        firstName: 'bar',
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    {
+                        dependsOn: ['author.firstName', 'category'],
+                        resolve: values => {
+                            return values.author.firstName === 'foo' && values.category === 'bar';
+                        },
+                    },
+                ),
+            ).toEqual({ show: false });
+        });
+
+        it('returns { show: true } if the resolve function returns true', () => {
+            expect(
+                mapStateToProps(
+                    {
+                        form: {
+                            'record-form': {
+                                values: {
+                                    category: 'bar',
+                                    author: {
+                                        firstName: 'foo',
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    {
+                        dependsOn: ['author.firstName', 'category'],
+                        resolve: values => {
+                            return values.author.firstName === 'foo' && values.category === 'bar';
+                        },
+                    },
+                ),
+            ).toEqual({ show: true });
+        });
+    });
+});
+
+describe('<DependentInput />', () => {
+    it('returns null when show prop is false', () => {
+        const wrapper = shallow(
+            <DependentInput show={false}>
+                <span />
+            </DependentInput>,
+        );
+        expect(wrapper.type() === null);
+    });
+
+    it('returns a unique FormField element when passed a unique child', () => {
+        const wrapper = shallow(
+            <DependentInput show={true}>
+                <span source="aSource" />
+            </DependentInput>,
+        );
+
+        expect(wrapper.name()).toEqual('div');
+        expect(wrapper.prop('className')).toEqual('aor-input-aSource');
+        const formFields = wrapper.find('FormField');
+        expect(formFields.length).toEqual(1);
+        expect(formFields.at(0).prop('input')).toEqual(<span source="aSource" />);
+    });
+
+    it('returns a span with FormField children for each passed child', () => {
+        const wrapper = shallow(
+            <DependentInput show={true}>
+                <span className="1" />
+                <span className="2" />
+                <span className="3" />
+            </DependentInput>,
+        );
+
+        expect(wrapper.at(0).type()).toEqual('div');
+        const formFields = wrapper.find('FormField');
+        expect(formFields.length).toEqual(3);
+
+        expect(formFields.at(0).prop('input')).toEqual(<span className="1" />);
+        expect(formFields.at(1).prop('input')).toEqual(<span className="2" />);
+        expect(formFields.at(2).prop('input')).toEqual(<span className="3" />);
+    });
+});
